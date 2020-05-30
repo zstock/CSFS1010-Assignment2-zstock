a7922611447c1a1c639ff708a50872a48427a9f7
zstock <Zach.J.Stock@gmail.com>
2020-05-30 15:48:14 -0400

    Initial Commit
    Can add items to list. Can check items off. Checked off items get greyed out.

---
+++ Functions.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    //When the new item form is submitted. 
+    $("#newItemInput").submit(function( event ) {
+        event.preventDefault();
+        let newItemContent = $("#newItem").val();//Get the content of the input field.
+        $("#newItem").val('');//Reset the input field for future input.
+        AddItemToList(newItemContent, "#list");
+    });
+
+    $(document).on('change', '[type=checkbox]', function (){
+        console.log ($(this));
+        let parent = $(this).parent();
+        if ($(this).is(":checked"))
+        {
+            $(this).siblings().addClass("checked");
+
+        }
+        else
+        {
+            $(this).siblings().removeClass("checked");
+        }
+    });
+
+    //This will add the input item to the Todo List.
+    //newItem: Content to be added into the todo list.
+    //listID: Allow this function to add content to different lists, if I ever expand it to have such functionality.
+    function AddItemToList(newItem, listID){
+        let existingHtml = $(listID).html();
+        $(listID).html(`${existingHtml}<li><input type="checkbox"><label> ${newItem}</labe></li>`);
+        //UpdateCheckboxList();
+    }
+});
